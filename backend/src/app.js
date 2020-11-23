2e291c6c1b883f5ef94bdbe8b244b68fbf78c2cd
Helari <helari.sosi@gmail.com>
2020-11-23 15:46:07 +0100

    Add cors & test api

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const createError = require('http-errors');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 const logger = require('morgan');
 const helmet = require('helmet');
 
@@ -15,6 +16,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 
 app.use('/', indexRouter);
 
