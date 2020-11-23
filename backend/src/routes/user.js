653c80958aa669788388f4124dbb3914ff556e3c
Georg Ekeberg <georghagen@gmail.com>
2020-11-23 19:11:39 +0100

    added get route for all users /users

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -2,7 +2,7 @@ const User = require('../models/user')
 const express = require('express')
 const router = new express.Router()
 
-router.post("/user", async (req, res) => {
+router.post("/users", async (req, res) => {
   console.log(req.body);
 
   const { username, password, firstname, lastname } = req.body
@@ -15,6 +15,14 @@ router.post("/user", async (req, res) => {
   await user.save(function (err, user) {
     if (err) return console.error(err);
   });
+
+  User.find((err, users) => {
+    if (err) return console.error(err)
+    res.send(users)
+  })
+
+});
+router.get("/users", async (req, res) => {
 
   User.find((err, users) => {
     if (err) return console.error(err)
