ad24c2b4c36d02ca8da2f824ef7a1290d3ee8fd3
Joakim-Andersson <jjoakimm@hotmail.com>
2020-11-24 15:07:03 +0100

    Linting check

--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -3,55 +3,57 @@ import TextField from "@material-ui/core/TextField"
 import Button from "@material-ui/core/Button"
 
 function Login() {
-const [nameInput, setNameInput] = useState("")
-const [passwordInput, setPasswordInput] = useState("")
+  const [nameInput, setNameInput] = useState("")
+  const [passwordInput, setPasswordInput] = useState("")
 
-const handleNameInputChange = (e) => {
+  const handleNameInputChange = (e) => {
     const inputText = e.target.value
     setNameInput(inputText)
-}
+  }
 
-const handlePasswordInputChange = (e) => {
+  const handlePasswordInputChange = (e) => {
     const inputText = e.target.value
     setPasswordInput(inputText)
-}
+  }
 
-const logInSubmit = (e) => {
+  const logInSubmit = () => {
     if (!nameInput || !passwordInput) {
-        return
-    } 
+      console.log("Please fill in fields")
+    }
     // else LOG IN!!!?
-}
+  }
 
-return (
+  return (
     <div>
-        <form className="namefield" onSubmit={logInSubmit}>
-            <TextField
-                required
-                type="text"
-                className="namefield"
-                id="filled-required"
-                label="Enter name"
-                variant="outlined"
-                value={input}
-                onChange={handleNameInputChange}
-            />
-        </form>
-        <form className="passwordfield" onSubmit={logInSubmit}>
-            <TextField
-                required
-                type="text"
-                className="namefield"
-                id="filled-required"
-                label="Enter password"
-                variant="outlined"
-                value={input}
-                onChange={handlePasswordInputChange}
-            />
-        </form>
-        <Button variant="contained" onClick={logInSubmit}>LOG IN</Button>
+      <form className="namefield" onSubmit={logInSubmit}>
+        <TextField
+          required
+          type="text"
+          className="namefield"
+          id="filled-required"
+          label="Enter name"
+          variant="outlined"
+          value={nameInput}
+          onChange={handleNameInputChange}
+        />
+      </form>
+      <form className="passwordfield" onSubmit={logInSubmit}>
+        <TextField
+          required
+          type="text"
+          className="namefield"
+          id="filled-required"
+          label="Enter password"
+          variant="outlined"
+          value={passwordInput}
+          onChange={handlePasswordInputChange}
+        />
+      </form>
+      <Button variant="contained" onClick={logInSubmit}>
+        LOG IN
+      </Button>
     </div>
-)
+  )
 }
 
 export default Login
