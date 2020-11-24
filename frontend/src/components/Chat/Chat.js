05f305d5b646600402a330edfd483a246265fea8
Unknown <adrian.nesvik@lyse.net>
2020-11-24 11:05:17 +0100

    Add message list and empty input check

--- frontend/src/components/Chat/Chat.js
+++ frontend/src/components/Chat/Chat.js
@@ -1,31 +1,39 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import socketClient from 'socket.io-client'
 
 const Chat = () => {
-  const [socket, setSocket] = useState(undefined);
+  const socket= useRef();
   const [message, setMessage] = useState("");
-
-  useEffect(() => {
-    const socketIO = socketClient('http://localhost:8000/');
-    socketIO.on('connect', () => {
-      console.log("Connect");
-    })
-    socketIO.on('disconnect', () => {
-      console.log("Disconnect");
-    })
-    setSocket(socketIO)
-  }, [])
-
+  const [messageList, setMessageList] = useState([]);
+  
   const handleSubmit = e => {
     e.preventDefault();
-    socket.emit('message', message);
-    setMessage("");
+    if(message !== "") {
+      socket.current.emit('message', message);
+      setMessage("");
+    }
   }
+
+  useEffect(() => {
+    socket.current = socketClient('http://localhost:8000/');
+    socket.current.on('connect', () => {
+      console.log("Connect");
+    })
+    socket.current.on('disconnect', () => {
+      console.log("Disconnect");
+    })
+    socket.current.on('message', msg => {
+      setMessageList(prevMessageList => [...prevMessageList, msg])
+    })
+  }, [])
 
   if(!socket) return <h1>Establishing connection...</h1>
 
   return (
     <section>
+      <ul>
+        {messageList.map(message => <li>{message}</li>)}
+      </ul>
       <form onSubmit={handleSubmit}>
         <input type="text" value={message} onChange={e => setMessage(e.target.value)}/>
         <input type="submit" value="Send"/>
