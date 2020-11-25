4e9d5d736f20ffb1b53ac1cf4b27d6ef57cc017b
Joakim-Andersson <jjoakimm@hotmail.com>
2020-11-25 10:06:36 +0100

    Start group component

---
+++ frontend/src/components/Groups/Groups.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react"
+
+function Groups() {
+    const [groups, setGroups] = useState([]);
+
+    return (
+        <div>
+            <h1>Find a group you like here:</h1>
+        </div>
+    )
+}
+
+export default Groups
