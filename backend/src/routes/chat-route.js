36bcb87370cac435c572774387c51924f24ec0c3
nesadrian <adrian.nesvik@lyse.net>
2020-11-25 19:12:45 +0100

    Add message route

--- backend/src/routes/chat-route.js
+++ backend/src/routes/chat-route.js
@@ -1,15 +1,11 @@
 const express = require('express');
 
-const { createChat } = require('../controllers/chat-controller');
+const { createChat, addMessage } = require('../controllers/chat-controller');
 
 const router = express.Router();
 
 router.post('/', createChat);
 
-router.get('/', async (req, res) => {
-
-  //createChat(req, res, membersIds);
-  res.send("hello")
-});
+router.post('/message', addMessage);
 
 module.exports = router;
