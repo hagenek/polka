0acbd4808a98101a441c2d851825a74818dd7a58
Adrian Nesvik <adrian.nesvik@lyse.net>
2020-11-29 18:08:03 +0100

    Add clickable create chat icon

--- frontend/src/components/ChatList/ChatList.js
+++ frontend/src/components/ChatList/ChatList.js
@@ -3,12 +3,13 @@ import Chat from '../Chat/Chat'
 import ChatCard from '../ChatCard/ChatCard'
 import ChatForm from '../ChatForm/ChatForm'
 import api from "../../api"
+import CreateChatIcon from "@material-ui/icons/AddComment"
 import './ChatList.css'
 
 const ChatList = ({ userId }) => {
   const [chats, setChats] = useState(undefined)
   const [clickedChat, setClickedChat] = useState(undefined)
-  const [clickedCreateChat, setClickedCreateChat] = userState(false)
+  const [createChat, setCreateChat] = useState(false)
 
   useEffect(() => {
     const getChats = async () => {
@@ -21,9 +22,16 @@ const ChatList = ({ userId }) => {
   // Add better response page
   if(!chats) return <h1>No chats</h1>
 
+  if(createChat) {
+    return (
+      <h1>test</h1>
+    )
+  }
+
   return (
     <section className="chatlist__section">
       <section className="chatcard__container">
+          <CreateChatIcon onClick={() => setCreateChat(true)} />
           {chats.map(chat => <ChatCard key={chat._id} userId={userId} handleClick={chat => setClickedChat(chat)} chat={chat} /> )}
       </section>
       <section className="chat__container">
