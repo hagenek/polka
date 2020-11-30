5d095a61f11ace44e4d164a3d79fd0445199c34f
Joakim-Andersson <jjoakimm@hotmail.com>
2020-11-30 10:53:22 +0100

    Minor Refactoring

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,6 @@
 import React, { useState, useEffect } from "react"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import backend from "./api"
-// import backend from "./api"
 import About from "./components/About/About"
 import Search from "./components/Search/Search"
 import Nav from "./components/Nav/Nav"
