1f53851e6d8d8a5a84575522dcf198daf65fbde4
Helari <helari.sosi@gmail.com>
2020-12-01 11:28:26 +0100

    A

--- backend/src/controllers/user-controller.js
+++ backend/src/controllers/user-controller.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const User = require('../models/user');
+const { use } = require('../routes');
+const sharp = require("sharp");
 
 const userBoard = (req, res) => {
   res.status(200).send({ message: "User authenticated woop" });
@@ -7,12 +9,12 @@ const userBoard = (req, res) => {
 
 const getUser = async (req, res) => {
   let { id } = req.params;
-  
+
   try {
     id = mongoose.Types.ObjectId(id);
     const user = await User.findById(id);
     res.status(200).json(user);
-  } catch(error) {
+  } catch (error) {
     console.log(error);
     res.send(error.message)
   }
@@ -20,24 +22,24 @@ const getUser = async (req, res) => {
 
 const updateUser = async (req, res) => {
   const updates = Object.keys(req.body)
-  
-  // TODO : Implement valdiation of what you are allowed to change
-	// const allowedUpdates = ['firstName', 'lastName', 'interests', 'age']
-	// const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
 
-	// if (!isValidOperation) {
-	// 		return res.status(400).send({ error: 'Invalid updates!' })
+  // TODO : Implement valdiation of what you are allowed to change
+  // const allowedUpdates = ['firstName', 'lastName', 'interests', 'age']
+  // const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+  // if (!isValidOperation) {
+  // 		return res.status(400).send({ error: 'Invalid updates!' })
   // }
-	try {
-      let { id } = req.params;
+  try {
+    let { id } = req.params;
     id = mongoose.Types.ObjectId(id);
     const user = await User.findById(id);
-			updates.forEach((update) => user[update] = req.body[update])
-			await user.save()
-      res.send(user)
-	} catch (e) {
-			res.status(400).send(e)
-	}
+    updates.forEach((update) => user[update] = req.body[update])
+    await user.save()
+    res.send(user)
+  } catch (e) {
+    res.status(400).send(e)
+  }
 }
 
 const getAllUsers = async (req, res) => {
@@ -49,15 +51,15 @@ const getAllUsers = async (req, res) => {
 
 const getImageFromUser = async (req, res) => {
   let { id } = req.params;
-  
+
   try {
     id = mongoose.Types.ObjectId(id);
     const user = await User.findById(id);
     res.status(200)
-    res.send(user.images)
-  } catch(error) {
+    res.send(user.avatar)
+  } catch (error) {
     console.log(error);
-}
+  }
 }
 
 const addUser = async (req, res) => {
@@ -75,10 +77,23 @@ const addUser = async (req, res) => {
     await user.save()
 
     res.statusCode(200).json(user);
-  } catch(error) {
+  } catch (error) {
     console.log(error);
     res.send(error.message)
   }
+}
+
+const getByUsername = async (req, res) => {
+  const { username } = req.params;
+
+  try {
+    const user = await User.find({ username: `${username}` });
+    res.status(200).send(user);
+  } catch (error) {
+    console.log(error.message)
+    res.send(error.message);
+  }
+
 }
 
 module.exports = {
@@ -87,5 +102,6 @@ module.exports = {
   addUser,
   getAllUsers,
   getImageFromUser,
-  updateUser
+  updateUser,
+  getByUsername,
 }
