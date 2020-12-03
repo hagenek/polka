04934ad60e3d8d173100a7803aa48db69ec2eb90
Joakim-Andersson <jjoakimm@hotmail.com>
2020-12-03 08:17:29 +0100

    Add header element to games-component

--- frontend/src/components/Games/Games.js
+++ frontend/src/components/Games/Games.js
@@ -3,32 +3,39 @@ import { Link } from "react-router-dom"
 import SnakeImg from "../../assets/snake.png"
 import Memory from "../../assets/memory.png"
 
-import "./Games.css"
+import GameImg from "../../assets/games.png"
 
+import "./Games.css"
 
 function Games() {
     return (
-        <div className="game__div" >
-            <section className="game__section">
-                <h1>Find a game you like here:</h1>
-            </section>
-            <Link className="linkToGames" to="/memory">
-                <h3 className="game__name">Memory:</h3>
-                <img
-                    className="game__image"
-                    src={Memory}
-                    alt="memory game"
-                />
-            </Link>
-            <Link className="linkToGames" to="/snake">
-            <h3 className="game__name">Snake:</h3>
-                <img
-                    className="game__image"
-                    src={SnakeImg}
-                    alt="snake game"
-                />
-            </Link>
-        </div>
+        <>
+            <div className="games__header">
+                <h1>Find new events</h1>
+                <img src={GameImg} alt="event image" />
+            </div>
+            <div className="game__div" >
+                <section className="game__section">
+                    <h1>Find a game you like here:</h1>
+                </section>
+                <Link className="linkToGames" to="/memory">
+                    <h3 className="game__name">Memory:</h3>
+                    <img
+                        className="game__image"
+                        src={Memory}
+                        alt="memory game"
+                    />
+                </Link>
+                <Link className="linkToGames" to="/snake">
+                    <h3 className="game__name">Snake:</h3>
+                    <img
+                        className="game__image"
+                        src={SnakeImg}
+                        alt="snake game"
+                    />
+                </Link>
+            </div>
+        </>
     )
 }
 
